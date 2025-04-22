4857231661829b2fd79b675e2281c2c8e238be34
Arthur Moura <mouraarthur625@gmail.com>
2025-04-22 14:29:45 -0300

    feat(main): Logging adicionado no arquivo

--- pedidos_api/main.py
+++ pedidos_api/main.py
@@ -1,4 +1,10 @@
 from processador import processar_pedidos
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 if __name__ == '__main__':
     processar_pedidos()
